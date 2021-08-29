60ac6ed6603fb70ddeff5c1a967f1f0727819858
sezinlemece <scoskunsu@yahoo.com>
2021-08-29 14:01:32 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {
+          final player = AudioCache();
+          playSound(soundNumber);
+        },
+        child: Text(""),
+        style: ElevatedButton.styleFrom(
+          primary: color,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Container(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.green, soundNumber: 3),
+              buildKey(color: Colors.teal, soundNumber: 4),
+              buildKey(color: Colors.yellow, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
